714b63a2e29fbbad9cd7c3c573d1afa89b587a52
Guilherme <carvalho_guilhermegomes@hotmail.com>
2022-11-17 12:27:04 -0300

    deixando o numero de reservar adicionada funcional

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import './style.css';
-import {MdDelete} from 'react-icons/md';
+import React from "react";
+import { useSelector } from "react-redux";
+import "./style.css";
+import { MdDelete } from "react-icons/md";
 
 function Reservas() {
+  const reserves = useSelector((state) => state.reserve); //variavel que armazena o numero de reservas feitas
+
+  console.log("MINHAS RESERVAS: ", reserves);
   return (
     <div>
-      <h1 className='title'>Você Solicitou 1 reserva</h1>
+      <h1 className="title">Você Solicitou {reserves.length}</h1>
 
-      <div className='reservas'>
-        <img
-        src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
-        alt='maceio' />
+      {reserves.map((reserve) => (
+        <div className="reservas" key={reserve.id}>
+          <img
+            src={reserve.image}
+            alt={reserve.title}
+          />
 
-        <strong>Viagem Maceio 7 dias</strong>
-        <span>Quantidade: 2</span>
+          <strong>{reserve.title}</strong>
+          <span>Quantidade: 2</span>
 
-        <button
-        type='button'
-        onClick={() => {}} >
-          <MdDelete size={20} color='#191919' />
-        </button>
+          <button type="button" onClick={() => {}}>
+            <MdDelete size={20} color="#191919" />
+          </button>
         </div>
+      ))}
 
-        <footer>
-          <button type='button'>Solicitar Reservas</button>
-        </footer>
-
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Reservas
+export default Reservas;
